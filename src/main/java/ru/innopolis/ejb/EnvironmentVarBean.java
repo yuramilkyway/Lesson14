47e0de059acb43c5963debdac2de32b64cd2cd5a
YuraVolkov <yuramilkyway@gmail.com>
2020-10-20 21:14:41 +0300

    Добавление сортировки списка с переменными среды.

--- src/main/java/ru/innopolis/ejb/EnvironmentVarBean.java
+++ src/main/java/ru/innopolis/ejb/EnvironmentVarBean.java
@@ -1,16 +1,29 @@
 package ru.innopolis.ejb;
 
 import javax.ejb.Stateless;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
+/**
+ * Создаем бин, для получения системных переменных
+ */
 @Stateless
 public class EnvironmentVarBean {
-    public Set<Map.Entry<String, String>> getAllVariables() {
-        //Можно отсортировать через список
-        return System.getenv().entrySet();
+    /**
+     * Получаем список всех системных переменных и сорируем по алфавиту.
+     * @return Отсортированный лист с системными переменными.
+     */
+    public Collection<Map.Entry<String, String>> getAllVariables() {
+        Set<Map.Entry<String, String>> set = System.getenv().entrySet();
+        List<Map.Entry<String, String>> list = new ArrayList<>(set);
+        list.sort(Map.Entry.comparingByKey());
+        return list;
     }
 
+    /**
+     * Получаем значение у заданной системной переменной.
+     * @param name Имя системной переменной.
+     * @return Значение системной переменной.
+     */
     public String getValueVariable(String name) {
         return System.getenv(name);
     }
